f892cc83cff14f95ac58097681b9fea326cb4572
Anna Levina <anna_levina@internet.ru>
2025-10-29 09:16:06 +0300

    refactor: удалены коменты

--- pep_parse/items.py
+++ pep_parse/items.py
@@ -1,15 +1,7 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class PepParseItem(scrapy.Item):
-    # номер PEP (например, 8)
     number = scrapy.Field()
-    # название PEP (например, The Style Guide for Python Code)
     name = scrapy.Field()
-    # статус PEP (например, Final)
     status = scrapy.Field()
